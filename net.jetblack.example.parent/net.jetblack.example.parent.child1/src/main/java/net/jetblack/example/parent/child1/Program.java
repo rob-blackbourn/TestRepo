b839d9ac3abe6eebe19950f27d7c18f23e02cce1
Rob Blackbourn <rblackbourn@ugsb-rbla01.bhdgsystematic.com>
2019-04-29 07:34:35 +0100

    Initial commit

---
+++ net.jetblack.example.parent/net.jetblack.example.parent.child1/src/main/java/net/jetblack/example/parent/child1/Program.java
@@ -0,0 +1,10 @@
+package net.jetblack.example.parent.child1;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ net.jetblack.example.parent/net.jetblack.example.parent.child2/src/main/java/net/jetblack/example/parent/child2/Program.java
@@ -0,0 +1,10 @@
+package net.jetblack.example.parent.child2;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
